7144ccd905d7451804de31216fa8166b75c286ea
Edouard lamotte <elamotte@sedona.fr>
2020-10-31 20:25:44 +0100

    set up sql script

--- kubernetes-quickstart/src/main/resources/import.sql
+++ kubernetes-quickstart/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, TASTED) VALUES (NEXTVAL(hibernate_sequence), 'Chouffe', 'Yellow', false);
-INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, TASTED) VALUES (NEXTVAL(hibernate_sequence), 'Chimay', 'Amber', false);
-INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, TASTED) VALUES (NEXTVAL(hibernate_sequence), 'Kwack', 'Amber', false);
-INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, TASTED) VALUES (NEXTVAL(hibernate_sequence), 'Ch''ti Triple','Yellow', false);
-INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, TASTED) VALUES (NEXTVAL(hibernate_sequence), 'Goudale', 'Amber', false);
+INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, BEER_PRODUCER, BEER_COUNTRY, BEER_PRICE) VALUES (NEXTVAL(hibernate_sequence), 'Chouffe', 'yellow', '', '', 0);
+INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, BEER_PRODUCER, BEER_COUNTRY, BEER_PRICE) VALUES (NEXTVAL(hibernate_sequence), 'Chimay', 'amber', '', '', 0);
+INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, BEER_PRODUCER, BEER_COUNTRY, BEER_PRICE) VALUES (NEXTVAL(hibernate_sequence), 'Kwack', 'amber', '', '', 0);
+INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, BEER_PRODUCER, BEER_COUNTRY, BEER_PRICE) VALUES (NEXTVAL(hibernate_sequence), 'Ch''ti Triple','yellow', '', '', 0);
+INSERT INTO BEER_TABLE (ID, BEER_NAME, BEER_COLOR, BEER_PRODUCER, BEER_COUNTRY, BEER_PRICE) VALUES (NEXTVAL(hibernate_sequence), 'Goudale', 'amber', '', '', 0);
