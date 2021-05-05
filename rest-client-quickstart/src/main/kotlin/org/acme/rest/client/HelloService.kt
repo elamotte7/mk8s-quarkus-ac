78fe8a396c1a079f1d0870f7d7e46d40c9d0eb80
Edouard lamotte <elamotte@sedona.fr>
2021-05-05 19:18:58 +0200

    bumpt to quarkus 1.13.3.Final

--- rest-client-quickstart/src/main/kotlin/org/acme/rest/client/HelloService.kt
+++ rest-client-quickstart/src/main/kotlin/org/acme/rest/client/HelloService.kt
@@ -6,11 +6,10 @@ import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
-
+@Path("/hello")
 @RegisterRestClient
 interface HelloService {
     @GET
-    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     fun hello(): String?
 }
